eb193b7c7803a4125644aa4de22930f4f8c64815
danielDaleman <32171194+danielDaleman@users.noreply.github.com>
2018-02-21 22:42:01 -0500

    Update OrdersController.js

--- src/main/resources/static/js/OrdersController.js
+++ src/main/resources/static/js/OrdersController.js
@@ -1,17 +1,12 @@
 orders = undefined;
 
-
-
-addOrder = function(){
-	var insert = {2:{"orderAmountsMap":{"HOTDOG":10,"HAMBURGUER":20,"BEER":40},"tableNumber":2}};
+function addOrder(){
+	var insert = {2:{"orderAmountsMap":{"TOMATE":5,"LECHUGA":3,"POKER":15},"tableNumber":2}};
 	axios.post('/orders', insert)
-		.then(function(){                
-			//Render the tables
-                        $("#tablasActuales").append("<p id='tag"+2+"'>Order 2</p>");
-                        //$("#tablasActuales").append("<p id='total"+2+"'> total = "+getTotalBill(2)+"</p>");
+		.then(function(){                			
+                        $("#tablasActuales").append("<p id='tag"+2+"'>Order 2</p>");                        
 			$("#tablasActuales").append("<table id='Order"+2+"' class='table table-dark'> <thead> <tr> <th scope='col'>Product</th> <th scope='col'>Quantity</th> </tr> </thead>");
-			for(map in insert[2].orderAmountsMap){
-				//Render the rows
+			for(map in insert[2].orderAmountsMap){				
 				$("#Order"+2).append("<tbody> <tr> <td>"+map+"</td> <td>"+insert[2].orderAmountsMap[map]+"</td> </tr> </tbody>");
 			}
 			
@@ -22,11 +17,9 @@ addOrder = function(){
 		});
 }
 
-
-removeOrderById = function(id){
+function removeOrderById(id){
 	axios.delete('/orders/'+id)
-		.then(function(){
-                        //document.getElementById("total"+id).remove();
+		.then(function(){                        
                         document.getElementById("tag"+id).remove();
 			document.getElementById("Order"+id).remove();
 		})
@@ -36,7 +29,7 @@ removeOrderById = function(id){
 		});
 }
 
-loadOrdersList = function(){
+ function loadOrdersList(){
 	orders = [];
 	axios.get('/orders')
 		.then(function(result){
@@ -44,16 +37,13 @@ loadOrdersList = function(){
 			$("#tablasActuales").empty();
 			for(key in orders){
 				//Render the tables
-                                $("#tablasActuales").append("<p id='tag"+key+"'>Order "+key+ "</p>");
-                                //$("#tablasActuales").append("<p id='total"+key+"'> total = "+getTotalBill(key)+"</p>");
+                                $("#tablasActuales").append("<p id='tag"+key+"'>Order "+key+ "</p>");                                
 				$("#tablasActuales").append("<table id='Order"+key+"' class='table table-dark'> <thead> <tr> <th scope='col'>Product</th> <th scope='col'>Quantity</th> </tr> </thead>");
 				for(map in orders[key].orderAmountsMap){
 					//Render the rows
 					$("#Order"+key).append("<tbody> <tr> <td>"+map+"</td> <td>"+orders[key].orderAmountsMap[map]+"</td> </tr> </tbody>");
 				}
-			}
-			
-			//console.log(orders);
+			}						
 		})
 		.catch(function(error){
 			console.log(error);
@@ -62,7 +52,7 @@ loadOrdersList = function(){
 }
 
 
-errorMessage = function(){
+function errorMessage(){
 	alert("There is a problem with our servers. We apologize for the inconvince, please try again later");
 }
 
