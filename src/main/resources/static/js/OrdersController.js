38e30b67b4552e1c819f2a448ff7176864520380
danielDaleman <32171194+danielDaleman@users.noreply.github.com>
2018-02-21 22:52:00 -0500

    Update OrdersController.js

--- src/main/resources/static/js/OrdersController.js
+++ src/main/resources/static/js/OrdersController.js
@@ -35,12 +35,10 @@ function removeOrderById(id){
 		.then(function(result){
 			orders = result.data;
 			$("#tabla").empty();
-			for(key in orders){
-				//Render the tables
+			for(key in orders){				
                                 $("#tabla").append("<p id='tag"+key+"'>Order "+key+ "</p>");                                
 				$("#tabla").append("<table id='Order"+key+"' class='table table-dark'> <thead> <tr> <th scope='col'>Product</th> <th scope='col'>Quantity</th> </tr> </thead>");
-				for(map in orders[key].orderAmountsMap){
-					//Render the rows
+				for(map in orders[key].orderAmountsMap){					
 					$("#Order"+key).append("<tbody> <tr> <td>"+map+"</td> <td>"+orders[key].orderAmountsMap[map]+"</td> </tr> </tbody>");
 				}
 			}						
